2cb53b3fc6632ab504becae1fddd820361f13578
Kevin Duncke <kevinduncke@gmail.com>
2025-07-22 20:37:31 -0300

    KD Portafolio Update #2

--- index.js
+++ index.js
@@ -1,6 +1,34 @@
-//=============================================================//
+function fetchCertificates(param) {
+  fetch(`./data/${param}-certificates.json`)
+    .then((response) => response.json())
+    .then((certificates) => {
+      const list = document.getElementById(`${param}-row`);
 
+      certificates.forEach((cert) => {
+        const div = document.createElement("div");
+        div.classList.add("col-md-4");
+        div.innerHTML = `
+          <div class="card h-100 shadow-sm">
+            <a 
+              href="${cert.link}" 
+              target="_blank"
+              class="card-link"
+            >
+              <div class="card-body">
+                <img
+                  src="./res/PS-horizontal-color-fill-blue-text.png"
+                  alt="Pluralsight Banner"
+                  class="img-ps-banner"
+                />      
+                <h5 class="card-title">${cert.title}</h5>
+                <p class="card-text">${cert.date} | ${cert.time}</p>
+              </div>
+            </a>
+          </div>            
+        `;
 
-
-
-//=============================================================//
+        list.appendChild(div);
+      });
+    })
+    .catch((error) => console.error("Error fetching certificates: ", error));
+}
